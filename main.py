fa9f3629751af8575354ab0372ca62ef8b84b324
zhouzechu <68368705+zhouzechu@users.noreply.github.com>
2021-11-08 17:42:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,44 +11,44 @@ import load_data
 
 
 def train(path):
-    img_tr, label_tr = load_data.load_mnist(path, 'train')
+    img_tr, label_tr = load_data.load_mnist(path, 'train')  # load training data from dataset
 
-    ss = StandardScaler()
-    img_tr = ss.fit_transform(img_tr)
-    img_ts, label_ts = load_data.load_mnist(path, 'test')
-    img_ts_norm = ss.transform(img_ts)
+    ss = StandardScaler()  # instantiate the method of normalization
+    img_tr = ss.fit_transform(img_tr)  # normalize the training image and calculate the mean and variance
+    img_ts, label_ts = load_data.load_mnist(path, 'test')  # load testing data from dataset
+    img_ts_norm = ss.transform(img_ts)  # normalize the testing image
 
-    model = svm.LinearSVC()
-    model.fit(img_tr, label_tr)
+    model = svm.LinearSVC()  # construct model with different kernel function, you can use svm.SVC(kernel = 'rbf')/svm.SVC(kernel = 'poly') of radial basis kernel function and polynomial kernel function
+    model.fit(img_tr, label_tr)  # training
 
-    pre_label = model.predict(img_ts_norm)
+    pre_label = model.predict(img_ts_norm)  # obtain the result of model
 
     with open('./model_lin.pkl', 'wb') as file:
-        pickle.dump(model, file)
+        pickle.dump(model, file)  # save the model
 
     print('Accuracy:', np.sum(pre_label == label_ts) / label_ts.size)
-    print(classification_report(label_ts, pre_label))
+    print(classification_report(label_ts, pre_label))  # print the report with precision, recall, F1-Score, macro average AVG and weighted average AVG
     return img_ts, pre_label, label_ts
 
 
 def test(path):
-    img_ori = image.imread(path)
-    img_ori = img_ori.reshape(28, 28)
-    img_real = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)
-    img = img_real.reshape(1, 784)
-    ss = StandardScaler()
-    img = ss.fit_transform(img)
-    with open('./model_rbf.pkl', 'rb') as m:
-        model = pickle.load(m)
+    img_ori = image.imread(path)  # read real image
+    img_ori = img_ori.reshape(28, 28)  # reshape real image into 28*28 as same as the dateset
+    img_real = cv2.cvtColor(img_ori, cv2.COLOR_BGR2GRAY)  # transfer the channel of real image from 3 into 1
+    img = img_real.reshape(1, 784)  # reshape real image into 28*28=784 vector
+    ss = StandardScaler()  # normalization
+    img = ss.fit_transform(img)  # normalization
+    with open('./model_rbf.pkl', 'rb') as m:  # use the rb format reading model via relative path
+        model = pickle.load(m)  # load pretrained-model
 
-    predict_label = model.predict(img)
+    predict_label = model.predict(img)  # obtain result
     return img_ori, predict_label
 
 
 if __name__ == '__main__':
-    if argv[1] == 'train':
-        img_test, pre_label, label = train('C:/Users/PC/Desktop/SVM/')
+    if argv[1] == 'train':  # you can select different mode via command
+        img_test, pre_label, label = train('C:/Users/PC/Desktop/SVM/')  # train and test 
         utils.show_result(img_test, pre_label, label)
     elif argv[1] == 'test':
-        image_real, pre_label = test('C:/Users/PC/Desktop/SVM/4.png')
+        image_real, pre_label = test('C:/Users/PC/Desktop/SVM/4.png')  # validate on real image
         utils.show_real(image_real, pre_label)
